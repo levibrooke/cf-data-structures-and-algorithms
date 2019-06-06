25e2caf4e63f2dbcde3cbf6512a6fd06388f8f5b
Levi Porter <levi@levibrooke.com>
2019-06-06 11:06:14 -0700

    Add tests for different length lists

--- code-challenges/401/challenges/src/test/java/challenges/LinkedList/LinkedListTest.java
+++ code-challenges/401/challenges/src/test/java/challenges/LinkedList/LinkedListTest.java
@@ -191,4 +191,42 @@ public class LinkedListTest {
                 "LinkedList: 3 -> 7 -> 2 -> 6 -> 1 -> 5 -> null",
                 result.print());
     }
+
+    @Test
+    public void mergeLists_oneLongTest() {
+        LinkedList listOne = new LinkedList();
+        listOne.insert(1);
+        listOne.insert(2);
+        listOne.insert(3);
+
+        LinkedList listTwo = new LinkedList();
+        listTwo.insert(6);
+        listTwo.insert(7);
+
+        LinkedList result = LinkedList.mergeLists(listOne, listTwo);
+
+        assertEquals("merge lists",
+                "LinkedList: 3 -> 7 -> 2 -> 6 -> 1 -> null",
+                result.print());
+    }
+
+    @Test
+    public void mergeLists_twoLongTest() {
+        LinkedList listOne = new LinkedList();
+        listOne.insert(1);
+        listOne.insert(2);
+        listOne.insert(3);
+
+        LinkedList listTwo = new LinkedList();
+        listTwo.insert(8);
+        listTwo.insert(5);
+        listTwo.insert(6);
+        listTwo.insert(7);
+
+        LinkedList result = LinkedList.mergeLists(listOne, listTwo);
+
+        assertEquals("merge lists",
+                "LinkedList: 3 -> 7 -> 2 -> 6 -> 1 -> 5 -> 8 -> null",
+                result.print());
+    }
 }
