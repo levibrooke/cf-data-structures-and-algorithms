d2464f745afe59758ca0cdc7af56f27b1b69937a
Levi Porter <levi@levibrooke.com>
2019-06-10 11:52:15 -0700

    Add methods for queue, create test files

--- code-challenges/401/challenges/src/main/java/challenges/stacksandqueues/Queue.java
+++ code-challenges/401/challenges/src/main/java/challenges/stacksandqueues/Queue.java
@@ -1,4 +1,37 @@
 package challenges.stacksandqueues;
 
 public class Queue {
+    Node front;
+    Node rear;
+
+    public Queue() {
+        front = null;
+        rear = null;
+    }
+
+    public void enqueue(int value) {
+        Node node = new Node(value);
+
+        // if empty queue
+        if (front == null) {
+            front = node;
+            rear = node;
+        }
+
+        // if not empty
+        rear.next = node;
+        rear = node;
+    }
+
+    public int dequeue() {
+        Node current = front;
+        front = front.next;
+        current.next = null;
+
+        return current.data;
+    }
+
+    public int peek() {
+        return front.data;
+    }
 }
---
+++ code-challenges/401/challenges/src/test/java/challenges/stacksandqueues/StackTest.java
@@ -0,0 +1,20 @@
+package challenges.stacksandqueues;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackTest {
+
+    @Test
+    public void push() {
+    }
+
+    @Test
+    public void pop() {
+    }
+
+    @Test
+    public void peek() {
+    }
+}
