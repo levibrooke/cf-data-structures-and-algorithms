743ed887d37fc2af3dacd97f99fa6bc03c3c46ec
Levi Porter <levi@levibrooke.com>
2019-06-17 17:58:27 -0700

    Add inOrder and postOrder traversal methods

--- code-challenges/401/challenges/src/main/java/challenges/Tree/BinaryTree.java
+++ code-challenges/401/challenges/src/main/java/challenges/Tree/BinaryTree.java
@@ -14,8 +14,7 @@ public class BinaryTree<T extends Comparable> {
         return this.root == null ? true : false;
     }
 
-    // traversal methods go here
-
+    // preOrder traversal
     public ArrayList preOrder() {
         ArrayList preOrderList = new ArrayList();
 
@@ -28,11 +27,49 @@ public class BinaryTree<T extends Comparable> {
         if (node == null) {
             return;
         }
-        
+
         preOrderList.add(node.value);
 
         preOrder(node.left, preOrderList);
 
         preOrder(node.right, preOrderList);
+    }
+
+    // inOrder traversal
+    public ArrayList inOrder() {
+        ArrayList inOrderList = new ArrayList();
+
+        this.inOrder(this.root, inOrderList);
+
+        return inOrderList;
+    }
+
+    private void inOrder(BinaryTreeNode node, ArrayList inOrderList) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left, inOrderList); // move to left first
+        inOrderList.add(node.value);
+        inOrder(node.right, inOrderList);
+    }
+
+    // postOrder traversal
+    public ArrayList postOrder() {
+        ArrayList postOrderList = new ArrayList();
+
+        this.postOrder(this.root, postOrderList);
+
+        return postOrderList;
+    }
+
+    private void postOrder(BinaryTreeNode node, ArrayList postOrderList) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left, postOrderList); // move to left first
+        postOrder(node.right, postOrderList); // then right
+        postOrderList.add(node.value);
     }
 }
