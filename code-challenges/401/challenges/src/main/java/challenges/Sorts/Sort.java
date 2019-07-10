e90239f29f1e25698ac481602eda8ec184474b17
Levi Porter <levi@levibrooke.com>
2019-07-09 23:09:25 -0700

    Add quickSort and helper methods

--- code-challenges/401/challenges/src/main/java/challenges/Sorts/Sort.java
+++ code-challenges/401/challenges/src/main/java/challenges/Sorts/Sort.java
@@ -70,4 +70,37 @@ public class Sort {
 
         return inputArr;
     }
+
+    public static int[] quickSort(int[] inputArr, int left, int right) {
+        if (left < right) {
+            int position = partition(inputArr, left, right);
+
+            quickSort(inputArr, left, position - 1);
+
+            quickSort(inputArr, position + 1, right);
+        }
+        return inputArr;
+    }
+
+    public static int partition(int[] inputArr, int left, int right) {
+        int pivot = inputArr[right];
+        int low = left - 1;
+
+        for (int i = left; i < right; i++) {
+            if (inputArr[i] <= pivot) {
+                low++;
+                swap(inputArr, right, low + 1);
+            }
+        }
+
+        swap(inputArr, right, low + 1);
+
+        return low + 1;
+    }
+
+    public static void swap(int[] inputArr, int i, int low) {
+        int temp = inputArr[i];
+        inputArr[i] = inputArr[low];
+        inputArr[low] = temp;
+    }
 }
