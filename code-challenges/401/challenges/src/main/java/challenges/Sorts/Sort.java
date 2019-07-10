c1e50549751c60341745cf55de3da43fda531e40
Levi Porter <levi@levibrooke.com>
2019-07-09 18:25:09 -0700

    Add mergeSort method and helper

--- code-challenges/401/challenges/src/main/java/challenges/Sorts/Sort.java
+++ code-challenges/401/challenges/src/main/java/challenges/Sorts/Sort.java
@@ -1,5 +1,8 @@
 package challenges.Sorts;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Sort {
 
     public static int[] insertionSort(int[] inputArr) {
@@ -15,6 +18,50 @@ public class Sort {
 
             inputArr[j + 1] = temp;
         }
+        return inputArr;
+    }
+
+    public static int[] mergeSort(int[] inputArr) {
+        int length = inputArr.length;
+
+        if (length > 1) {
+            int mid = length / 2;
+
+            // sort left
+            int[] left = mergeSort(Arrays.copyOfRange(inputArr, 0, mid));
+
+            // sort right
+            int[] right = mergeSort(Arrays.copyOfRange(inputArr, mid, length));
+
+            // merge together
+            merge(left, right, inputArr);
+        }
+
+        return inputArr;
+    }
+
+    public static int[] merge(int[] left, int[] right, int[] inputArr) {
+        int i = 0; // left iterator
+        int j = 0; // right iterator
+        int k = 0; // inputArr iterator
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                inputArr[k] = left[i];
+                i++;
+            } else {
+                inputArr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        if (i == left.length) {
+            System.arraycopy(right, j, inputArr, k, right.length);
+        } else {
+            System.arraycopy(left, i, inputArr, k, left.length);
+        }
+
         return inputArr;
     }
 }
