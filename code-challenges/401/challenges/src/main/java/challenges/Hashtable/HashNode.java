6e1ca5360180c9606e0d4551e337677a6712c732
Levi Porter <levi@levibrooke.com>
2019-07-16 12:49:12 -0700

    Create hashnode class

---
+++ code-challenges/401/challenges/src/main/java/challenges/Hashtable/HashNode.java
@@ -0,0 +1,11 @@
+package challenges.Hashtable;
+
+public class HashNode<K, V> {
+    K key;
+    V value;
+
+    public HashNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
---
+++ code-challenges/401/challenges/src/main/java/challenges/Hashtable/HashTable.java
@@ -0,0 +1,4 @@
+package challenges.Hashtable;
+
+public class HashTable {
+}
